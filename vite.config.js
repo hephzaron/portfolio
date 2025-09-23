28234b622a369f7cbbdba7e9aaef911bb7b2e853
Daramola Tobi <tobi_daramola@yahoo.com>
2025-09-23 20:39:12 +0100

    create function to strip leading slash

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+  return {
+    
+    base: isProduction ? "/portfolio/" : "",
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
