030085cb81de15e66ce0292aa6ef3a57fe05d530
Daramola Tobi <tobi_daramola@yahoo.com>
2025-09-23 14:23:48 +0100

    refactor NotFound component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,15 @@
+// NotFound.js
+import React from "react";
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
-  return <div> NotFound</div>;
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Oops! Page not found.</h1>
+      <p>The page you're looking for does not exist.</p>
+      <Link to="/" style={{ color: "#007BFF", textDecoration: "underline" }}>
+        Go to Home
+      </Link>
+    </div>
+  );
 };
