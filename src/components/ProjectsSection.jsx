9715f9af440f1ece1a29767d2a70a13083bb0932
Daramola Tobi <tobi_daramola@yahoo.com>
2025-09-23 21:10:23 +0100

    add basename to brower router

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -67,9 +67,11 @@ export const ProjectsSection = () => {
               </div>
 
               <div className="p-6">
-                <div className="flex flex-wrap gap-2 mb-4">
-                  {project.tags.map((tag) => (
-                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                <div className="flex flex-wrap gap-2 mb-4" >
+                  {project.tags.map((tag, spkey) => (
+                    <span 
+                      className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground"
+                      key={spkey}>
                       {tag}
                     </span>
                   ))}
