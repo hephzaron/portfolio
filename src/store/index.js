30dac458a15cba6c38558cf0f4e9241cfca0c61a
Daramola Tobi <tobi_daramola@yahoo.com>
2025-09-25 21:47:23 +0100

    refactor store functional methods to clas method

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
-import skillReducer from './skillSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import skillReducer from "./skillSlice";
 
-export default configureStore({
-  reducer: {
-    skill: skillReducer
+/**
+ * @class ReduxStore
+ * @classdesc Responsible for configuring and creating the Redux store.
+ * Encapsulates store setup logic for better structure and extensibility.
+ */
+class ReduxStore {
+  /**
+   * @constructor
+   * Defines the mapping of reducers used by the Redux store.
+   */
+  constructor() {
+    /**
+     * Reducer map for the application.
+     * Add more slices here as the app grows.
+     * @type {object}
+     */
+    this.reducerMap = {
+      skill: skillReducer, // Skill slice reducer
+    };
   }
-})
+
+  /**
+   * Creates and configures the Redux store.
+   *
+   * @returns {import('@reduxjs/toolkit').EnhancedStore}
+   * Returns an enhanced Redux store instance ready for use with <Provider>.
+   */
+  createStore() {
+    return configureStore({
+      reducer: this.reducerMap
+    });
+  }
+}
+
+// ==================================================
+// Export a configured store instance for the Provider
+// ==================================================
+const storeInstance = new ReduxStore();
+const store = storeInstance.createStore();
+
+export default store;
